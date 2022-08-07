6ad765f5016adab43da2bfc94e77117600357657
Micheal-Winn <thant.zin.winnnn@gmail.com>
2022-08-07 14:30:01 +0630

    practice meta programming

--- practice.js
+++ practice.js
@@ -1,3 +1,37 @@
-async function hello(){
-    
+let obj = {
+    name : 'thantzinwin',
+    age : 20
 }
+
+function update()
+{
+    document.getElementById('name').innerText = obj.name;
+    document.getElementById('age').innerText = obj.age;
+}
+
+function lib(obj)
+{
+    let newObj = {};
+    for(let prop in obj)
+    {
+        console.log('property',prop);
+        Object.defineProperty(newObj,prop,{
+            get()
+            {
+                console.log('call getter',prop);
+                return obj[prop];
+            },
+            set(newValue)
+            {
+                console.log('call setter',prop);
+                obj[prop] = newValue;
+                update();
+            }
+        })
+    }
+    return newObj;
+}
+
+let newObj = lib(obj);
+newObj.age = 150
+console.log('age',newObj.age)
